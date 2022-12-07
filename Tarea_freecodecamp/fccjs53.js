eefdcc328abfcd47e43e0cbb26f167460736b7cf
alfredobrade <alfredobrade@gmail.com>
2022-12-06 22:27:38 -0300

    a

---
+++ Tarea_freecodecamp/fccjs53.js
@@ -0,0 +1,12 @@
+//53 local scope
+
+function myLocalScope() {
+    // Only change code below this line
+    let myVar;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
