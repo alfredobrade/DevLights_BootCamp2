6102b7fcbf96d38b63d287734c049fae9a4950ce
alfredobrade <alfredobrade@gmail.com>
2022-12-07 21:05:28 -0300

    a

---
+++ Tarea_freecodecamp/fccjs61.js
@@ -0,0 +1,11 @@
+// 62 comparison with the strict equality operator
+
+// Setup
+function testStrict(val) {
+    if (val===7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
---
+++ Tarea_freecodecamp/fccjs62.js
@@ -0,0 +1,11 @@
+// 62 practice comparing different values
+
+// Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
